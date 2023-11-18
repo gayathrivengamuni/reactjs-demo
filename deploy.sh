097c46c4f258d9754804149c33f08744f6ab4e9e
gayathrivengamuni <143080580+gayathrivengamuni@users.noreply.github.com>
2023-11-18 14:06:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
      ./build.sh
 
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u sravanaboyanagaythri -p $DOCKER_PASSWORD
+    docker login -u sravanaboyanagayathri -p $DOCKER_PASSWORD
 
   
     # Tag the image
@@ -19,7 +19,7 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
         docker build -t react-appimg .
 
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u sravanaboyanagaythri -p $DOCKER_PASSWORD
+    docker login -u sravanaboyanagayathri -p $DOCKER_PASSWORD
 
   
     # Tag the image
