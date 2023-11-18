c1adde4e94de365cd554399a38d69165098b9cc5
gayathrivengamuni <143080580+gayathrivengamuni@users.noreply.github.com>
2023-11-18 13:37:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-# Login to Docker Hub (dev repo)
-echo "$DOCKER_PASSWORD" | docker login -u "$sravanaboyanagayathri" --password-stdin
+# Check the Git branch
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    ./build.sh
 
-# Push the image to dev repository
-docker push sravanaboyanagayathri/dev-repo:latest
+    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
+    docker login -u sravanaboyanagaythri -p  dckr_pat_PiVQbOVkI-wXEPcDiMM8owlZZlM
+
+  
+    # Tag the image
+    docker tag react-appimg sravanaboyanagayathri/dev 
+  
+
+    # Push the image to the Dev Docker Hub repository
+    docker push sravanaboyanagayathri/dev
+  elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    ./build.sh
+
+    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
+    docker login -u sravanaboyanagaythri -p  dckr_pat_PiVQbOVkI-wXEPcDiMM8owlZZlM
+
+  
+    # Tag the image
+    docker tag react-appimg sravanaboyanagayathri/prod
+  
+
+    # Push the image to the Dev Docker Hub repository
+    docker push sravanaboyanagayathri/prod
+
